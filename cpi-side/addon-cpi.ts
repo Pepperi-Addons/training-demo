c511809b26d9a1ad201cbdf2add98db12433808e
Chasky Hoffmann <Chasky.h@pepperi.com>
2022-10-31 16:51:39 +0200

    added some load logic and a new endpoint

--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -5,12 +5,28 @@ import { AddonUUID } from '../addon.config.json'
 export async function load() {
     console.log('Here i am loading')
 
-
     const myTasks = await pepperi.api.adal.getList({
         addon: AddonUUID,
         table: 'my_tasks'
     })
     console.log("My tasks: ",  myTasks)
+
+    const t0 = performance.now();
+    const accounts = await pepperi.api.accounts.search({
+        fields: ['Name', 'UUID']
+    })
+    console.log("Accounts: ", JSON.stringify(accounts.objects))
+    
+    const t1 = performance.now();
+    const otherAccounts = await pepperi.papiClient.accounts.iter({
+        fields: ['Name', 'UUID']
+    }).toArray();
+    console.log("Other Accounts: ", JSON.stringify(otherAccounts))
+    
+    const t2 = performance.now()
+    console.log("Getting local accounts took: ", (t1-t0).toFixed(4), "ms")
+    console.log("Getting API accounts took: ", (t2-t1).toFixed(4), "ms")
+
 
 
     pepperi.events.intercept('RecalculateUIObject', {
@@ -30,9 +46,24 @@ export async function load() {
 }
 
 export const router = Router()
-router.get('/test', (req, res) => {
+router.get('/test', async (req, res) => {
     console.log(`${req.query.name} was here`)
-    res.json({
-        hello: req.query.name
+    
+
+    const tasks = await pepperi.addons.api.uuid(AddonUUID).get({
+        url: 'addon-cpi/my_tasks',
     })
+
+    res.json({
+        hello: req.query.name,
+        tasks: tasks.length
+    })
+})
+
+router.get('/my_tasks', async (req, res) => {
+    const myTasks = await pepperi.api.adal.getList({
+        addon: AddonUUID,
+        table: 'my_tasks'
+    })
+    res.json(myTasks.objects)
 })
