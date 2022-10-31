c480e710e0988f31e809e9cdf1e64fc58a1fc81d
Chasky Hoffmann <Chasky.h@pepperi.com>
2022-10-31 11:46:57 +0200

    Creating random home page buttons

--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -1,11 +1,27 @@
 import '@pepperi-addons/cpi-node'
+import { UIObject } from '@pepperi-addons/cpi-node';
 
 export async function load() {
-    
+    console.log('Here i am loading')
+    pepperi.events.intercept('RecalculateUIObject', {
+        UIObject: {
+            context: {
+                Name: 'UserHomePage'
+            }
+        }
+    }, async (context) => {
+        console.log("Here is my event")
+        const uiObject: UIObject = context.UIObject;
+
+        for (const field of uiObject.fields) {
+            field.visible = Math.random() > 0.5
+        }
+    })
 }
 
 export const router = Router()
 router.get('/test', (req, res) => {
+    console.log(`${req.query.name} was here`)
     res.json({
         hello: req.query.name
     })
