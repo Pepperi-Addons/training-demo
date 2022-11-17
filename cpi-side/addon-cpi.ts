0bfafa3c32a7cca2086720a2d3cfb339f7d26a9a
ChaskyH <chasky.h@pepperi.com>
2022-11-17 16:50:27 +0200

    added task-event route

--- cpi-side/addon-cpi.ts
+++ cpi-side/addon-cpi.ts
@@ -1,5 +1,7 @@
 import '@pepperi-addons/cpi-node'
 import { UIObject } from '@pepperi-addons/cpi-node';
+import { IClient } from '@pepperi-addons/cpi-node/build/cpi-side/events';
+import { setTimeout } from 'timers';
 import { AddonUUID } from '../addon.config.json'
 import { TasksService } from './tasks.service';
 
@@ -28,51 +30,29 @@ export async function load() {
     console.log("Getting local accounts took: ", (t1-t0).toFixed(4), "ms")
     console.log("Getting API accounts took: ", (t2-t1).toFixed(4), "ms")
 
-
-
-    // pepperi.events.intercept('RecalculateUIObject', {
-    //     UIObject: {
-    //         context: {
-    //             Name: 'UserHomePage'
-    //         }
-    //     }
-    // }, async (context) => {
-    //     console.log("Here is my event")
-    //     const uiObject: UIObject = context.UIObject;
-
-    //     for (const field of uiObject.fields) {
-    //         field.visible = Math.random() > 0.5
-    //     }
-    // })
-
-    pepperi.events.intercept('TasksEvent', {}, async (context) => {
-        console.log('in interceptor')
-        const service = new TasksService()
-        const tasks = await service.getTasks();
-        console.log(tasks)
-
-        const client = context.client;
-        
-        console.log("sending first client action")
-        await client?.alert("Hello", "We will start the task now")
-        console.log("after first client action")
-
-        await client?.showDialog({
-            title: 'Trick Or Treat!',
-            // content: "<html><img src='https://gray-weau-prod.cdn.arcpublishing.com/resizer/i24O6vVlm0PY8a571Tme_hiyFik=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/PPOT24VO3ZALPIIWSMKK6DQR5E.png'></img></html>",
-            content: 'You have to choose one..',
-            actions: [{
-                title: 'Trick',
-                value: false
-            },{
-                title: 'Treat',
-                value: true
-            }]
-        })
-
-        // return {
-        //     currentTask: tasks[0]
-        // }
+    pepperi.events.intercept('MyEvent', {}, async (context) => {
+        let res = {};
+        try {
+            
+            await context.client?.showDialog({
+                title: 'Trick Or Treat!',
+                // content: "<html><img src='https://gray-weau-prod.cdn.arcpublishing.com/resizer/i24O6vVlm0PY8a571Tme_hiyFik=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/PPOT24VO3ZALPIIWSMKK6DQR5E.png'></img></html>",
+                content: 'You have to choose one..',
+                actions: [{
+                    title: 'Trick',
+                    value: false
+                },{
+                    title: 'Treat',
+                    value: true
+                }]
+            })
+            // res = await pepperi.scripts.key(context.ScriptKey).run({}, context);
+            res = { "Hello": "World"}
+        }   
+        catch (err) {
+            res = { message: (err as Error).message}
+        }
+        return res;
     })
 }
 
@@ -97,4 +77,38 @@ router.get('/my_tasks', async (req, res) => {
         table: 'my_tasks'
     })
     res.json(myTasks.objects)
+})
+
+
+router.get('/task-event', async (req, res, next)=> {
+    console.log('in interceptor')
+        const service = new TasksService()
+        const tasks = await service.getTasks();
+        console.log(tasks)
+
+        const client = req.context?.client;
+        
+        console.log("sending first client action")
+        await client?.alert("Hello", "We will start the task now")
+        console.log("after first client action")
+
+        // await new Promise(resolve => setTimeout(resolve, 11000))
+        next(new Error('Some big failure'))
+
+        // await client?.showDialog({
+        //     title: 'Trick Or Treat!',
+        //     // content: "<html><img src='https://gray-weau-prod.cdn.arcpublishing.com/resizer/i24O6vVlm0PY8a571Tme_hiyFik=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/PPOT24VO3ZALPIIWSMKK6DQR5E.png'></img></html>",
+        //     content: 'You have to choose one..',
+        //     actions: [{
+        //         title: 'Trick',
+        //         value: false
+        //     },{
+        //         title: 'Treat',
+        //         value: true
+        //     }]
+        // })
+
+        // return {
+        //     currentTask: tasks[0]
+        // }
 })
